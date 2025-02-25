8c390713ab649ae64352a75a9660d6798c082f8f
Lu Win Maw <llaurence689@gmail.com>
2025-02-25 18:31:49 +0300

    Create fucntion for creating divs

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,13 @@
 const container = document.querySelector(".container");
 
-// function initiateGrids() {
-//     for (let i = 0; i <= 16; i++) {
-
-//     }
-// }
+function initiateGrids() {
+    
+    for (let i = 0; i <= 16; i++) {
+        for (let j = 0; k <= 16; j++) {
+            let grid = document.createElement("div");
+         
+            container.appendChild(grid);
+        }       
+    }
+    
+}
