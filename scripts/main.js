5b1297c3bd0d8b3dd70483f303d42a7c70e0b9ed
Lu Win Maw <llaurence689@gmail.com>
2025-02-25 21:20:36 +0300

    Add scaffolding for grids' event listeners

--- scripts/main.js
+++ scripts/main.js
@@ -2,18 +2,18 @@ const container = document.querySelector(".container");
 initiateGrids();
 const grids = document.querySelectorAll(".container div");
 
-// grids.forEach((aGrid)=> {
-//     aGrid.addEventListener("mouseover", (e) => {
-
-//     });
-// });
+grids.forEach((aGrid)=> {
+    aGrid.addEventListener("mouseover", (e) => {
+        aGrid.style.color = "";
+    });
+});
 
 function initiateGrids() {
     
     for (let i = 1; i <= 16; i++) {
         for (let j = 1; j <= 16; j++) {
             let grid = document.createElement("div");
-         
+            
             container.appendChild(grid);
         }       
     }
