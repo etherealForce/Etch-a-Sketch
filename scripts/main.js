7c64504d1ab3020fdfc0bc270d6643a34f5f17f4
Lu Win Maw <llaurence689@gmail.com>
2025-02-25 18:40:45 +0300

    Fix loop bug

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ const container = document.querySelector(".container");
 function initiateGrids() {
     
     for (let i = 0; i <= 16; i++) {
-        for (let j = 0; k <= 16; j++) {
+        for (let j = 0; j <= 16; j++) {
             let grid = document.createElement("div");
          
             container.appendChild(grid);
