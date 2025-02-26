e070e854bb1f510c6590cbb77e28ae4b1bb1b675
Lu Win Maw <llaurence689@gmail.com>
2025-02-26 14:49:19 +0300

    Fix grid bug

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,9 @@
 const container = document.querySelector(".container");
+const containerSize = 500;
 initiateGrids();
 const grids = document.querySelectorAll(".container div");
 const gridAdjustor = document.querySelector("#openPrompt");
-const containerSize = 500;
+
 gridAdjustor.addEventListener("click", (e) => {
 
 });
@@ -18,7 +19,7 @@ function initiateGrids(gridNum = 16) {
     for (let i = 1; i <= gridNum; i++) {
         for (let j = 1; j <= gridNum; j++) {
             let grid = document.createElement("div");
-            grid.style.cssText = `width: ${containerSize / gridNum}px; height: ${containerSize / gridNum}px`;
+            grid.style.cssText = `width: ${containerSize / (gridNum + 1)}px; height: ${containerSize / (gridNum + 1)}px`;
             container.appendChild(grid);
         }       
     }
